688e1fd994498206771e669c62bf637b0a77c62e
Fidel Castro <fidelcastro4080@gmail.com>
2025-12-24 17:44:00 +0300

    first project using API concept

---
+++ index.js
@@ -0,0 +1,31 @@
+// Pokemone Search
+
+
+async function fetchData(){
+
+    try{
+
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase().trim();//puts the name to lowercase and remove any spaces
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if(!response.ok){
+            throw new Error("Failed to fetch resource from the URL!")
+        }
+   
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+
+    }
+    catch(error){
+        console.error(error);
+    }
+    
+}
+
+fetchData();
+
